b56ca6e9e363b2581da8250613a12f25e93730c3
xbayazid <bayazid9006@gmail.com>
2023-08-23 21:17:06 +0600

    find doctor page search button added

--- src/Pages/Doctors/DoctorsSearch.js
+++ src/Pages/Doctors/DoctorsSearch.js
@@ -6,6 +6,18 @@ const DoctorsSearch = () => {
             <div className="text-center">
                 <h1 className='text-4xl font-bold'>Find a Doctor</h1>
                 <p className='text-2xl'>Find a healthcare provider at Medwin Cares</p>
+                <div className="flex items-center space-x-4 w-1/2 mx-auto mt-5">
+      <input
+        type="text"
+        className="py-2 px-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 flex-grow"
+        placeholder="Search..."
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-lg transition duration-300"
+      >
+        Search
+      </button>
+    </div>
             </div>
         </div>
     );
