5d9885c6b4f2497874330668f2c96a1ee596a14d
xbayazid <bayazid9006@gmail.com>
2024-01-11 10:16:16 +0600

    done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -18,6 +18,8 @@ const app = initializeApp(firebaseConfig);
 export default app;
 
 
+
+
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // // TODO: Add SDKs for Firebase products that you want to use
